7e7ae6db4d900cfb406e145e4380890e6013f371
zhanghongtian <zhanghongtian@chengjuiot.com>
2018-07-26 16:58:17 +0800

    add helloController

---
+++ src/main/java/com/chengjuiot/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.chengjuiot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	    @RequestMapping("/test/hello")
+	    @ResponseBody
+	    public String hello(){
+	        return "test hello spring";
+	    }
+}
